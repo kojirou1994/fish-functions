5ae632c28d2ae4bbaa120291e3df3e184ca3635e
kojirou <kojirouhtc@gmail.com>
2023-01-13 01:06:06 +0800

    fix echo issue

--- functions/proxy-on.fish
+++ functions/proxy-on.fish
@@ -1,9 +1,10 @@
 function proxy-on --argument server
-echo 
     set -gx http_proxy http://$server
     set -gx https_proxy $http_proxy
     set -gx all_proxy $http_proxy
     if status is-interactive
+        echo
         echo "Proxy Set To $http_proxy"
+        echo
     end
 end
