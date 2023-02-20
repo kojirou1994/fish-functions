e8d74bdc443b5917b291858099ccf6b245e92a3d
kojirou <kojirouhtc@gmail.com>
2023-02-20 19:06:57 +0800

    update proxy-on: set all_proxy to socks5
    proxy must be mixed-port

--- functions/proxy-on.fish
+++ functions/proxy-on.fish
@@ -1,7 +1,7 @@
 function proxy-on --argument server
     set -gx http_proxy http://$server
     set -gx https_proxy $http_proxy
-    set -gx all_proxy $http_proxy
+    set -gx all_proxy socks5://$server
     if status is-interactive
         echo
         echo "Proxy Set To $http_proxy"
