451cba3f64da3d9f62f2da112bff2bced0155e81
kojirou <kojirouhtc@gmail.com>
2024-05-04 10:04:06 +0800

    update k-alias

--- conf.d/k-alias.fish
+++ conf.d/k-alias.fish
@@ -1,6 +1,8 @@
-alias ll 'eza -la -@ -g -h --time-style long-iso'
-alias lt 'll -T'
+alias ll 'eza -l@gh --time-style long-iso'
 
 if test (uname) = Darwin
+    alias lt 'eza -la@ghT --time-style long-iso'
     alias ldd 'otool -L'
+else
+    alias lt 'eza -laghT --time-style long-iso'
 end
