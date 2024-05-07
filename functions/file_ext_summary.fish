0deb03a9b0238a982fbe22ea4be455a8b0839c1f
kojirou <kojirouhtc@gmail.com>
2024-05-07 16:28:08 +0800

    remove useless functions

--- functions/file_ext_summary.fish
+++
@@ -1,3 +0,0 @@
-function file_ext_summary
-    fd -t f -H . $argv | awk -F'.' '{print $NF}' | sort| uniq -c | sort -g
-end
--- functions/rsync-merge.fish
+++
@@ -1,3 +0,0 @@
-function rsync-merge
-    rsync -av --progress --ignore-existing --remove-source-files $argv
-end
