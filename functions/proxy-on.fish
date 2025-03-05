a66e9ae630b1d6b9e5e2f4b7970dbf66b17b1b2e
kojirou <kojirouhtc@gmail.com>
2025-03-05 19:07:07 +0800

    proxy-on: update

--- functions/proxy-on.fish
+++ functions/proxy-on.fish
@@ -1,10 +1,14 @@
 function proxy-on --argument server
-    set -gx http_proxy http://$server
-    set -gx https_proxy $http_proxy
-    set -gx all_proxy socks5://$server
+    set -gx http_proxy $server
+    set -gx https_proxy $server
+    set -gx all_proxy $server
+    set -gx HTTP_PROXY $server
+    set -gx HTTPS_PROXY $server
+    set -gx ALL_PROXY $server
+
     if status is-interactive
         echo
-        echo "Proxy Set To $http_proxy"
+        echo "Proxy Set To $server"
         echo
     end
 end
