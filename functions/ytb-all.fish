cc244ccd71ad53668fa9f66bb0e2c5a424ac599a
kojirou <kojirouhtc@gmail.com>
2025-04-29 01:20:45 +0800

    bye-bye chromium

--- functions/ytb-all.fish
+++ functions/ytb-all.fish
@@ -1,13 +1,13 @@
 function ytb-all
     for url in $argv
-        yt-dlp --cookies-from-browser chromium -f '(bestvideo[vcodec^=avc]/bestvideo)+(bestaudio[acodec^=mp4a]/bestaudio)' --write-subs --sub-langs all --no-overwrites $url
-        yt-dlp --cookies-from-browser chromium -f '(bestvideo[vcodec^=vp]/bestvideo)+(bestaudio[acodec=opus]/bestaudio)' $url
+        yt-dlp --cookies-from-browser brave -f '(bestvideo[vcodec^=avc]/bestvideo)+(bestaudio[acodec^=mp4a]/bestaudio)' --write-subs --sub-langs all --no-overwrites $url
+        yt-dlp --cookies-from-browser brave -f '(bestvideo[vcodec^=vp]/bestvideo)+(bestaudio[acodec=opus]/bestaudio)' $url
     end
     ytb-av1 $argv
 end
 
 function ytb-av1
     for url in $argv
-        yt-dlp --cookies-from-browser chromium -f '(bestvideo[vcodec^=av01])+(bestaudio[acodec=opus])' --merge-output-format mkv $url
+        yt-dlp --cookies-from-browser brave -f '(bestvideo[vcodec^=av01])+(bestaudio[acodec=opus])' --merge-output-format mkv $url
     end
 end
